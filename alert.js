eddd93d83a8ef63c406537575813ed57727db5df
he-he-he-ha-man <micah.2216223@nv.ccsd.net>
2026-04-25 08:24:54 -0700

    Delete alert.js

--- alert.js
+++
@@ -1,3 +0,0 @@
-window.onload = () => {
-  alert("if u need help email me, recommended settings: Fullbright, Keystrokes, Item Physics, Direction (to activate these press shift on the RIGHT side of the keyboard)");
-};
