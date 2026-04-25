1598eab3386cb91cd8c3a931f234a4becdc73ff2
he-he-he-ha-man <micah.2216223@nv.ccsd.net>
2026-04-24 23:01:49 -0700

    Add alert on window load with help message

---
+++ alert.js
@@ -0,0 +1,3 @@
+window.onload = () => {
+  alert("if u need help email me, recommended settings: Fullbright, Keystrokes, Item Physics, Direction (to activate these press shift on the RIGHT side of the keyboard)");
+};
